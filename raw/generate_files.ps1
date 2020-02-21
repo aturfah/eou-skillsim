d1e096a28f8ef434afdfafffb0ba9608f633276f
Ali Turfah <aturfah@gmail.com>
2020-02-21 18:27:03 -0500

    Update to move other files

--- raw/generate_files.ps1
+++ raw/generate_files.ps1
@@ -1,4 +1,5 @@
 ./download_pages.ps1
 python parser.py
-
 Move-Item skill_data.js ../src/skill_data.js -Force
+Move-Item prereq_data.js ../src/prereq_data.js -Force
+Move-Item mastery_skills.js ../src/mastery_skills.js -Force
