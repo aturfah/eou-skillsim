b85e3eff72bd754544b48e3e96e8eff6ca4f7fc7
Ali Turfah <aturfah@gmail.com>
2020-02-21 18:27:17 -0500

    Add new files

---
+++ src/mastery_skills.js
@@ -0,0 +1,3 @@
+var masterySkills = ["formula_mastery", "whip_mastery", "sword_mastery", "gun_mastery", "curse_mastery", "spear_mastery", "sword_mastery", "axe_mastery", "heal_mastery", "shield_mastery", "katana_mastery", "bow_mastery", "song_mastery"];
+    export default masterySkills;
+    
