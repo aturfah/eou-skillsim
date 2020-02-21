c2f59da2f335f528b5ae443258c35e2e3635a1f9
Ali Turfah <aturfah@gmail.com>
2020-02-21 13:35:33 -0500

    Read data files from folder

--- raw/parser.py
+++ raw/parser.py
@@ -1,10 +1,19 @@
 """Script to parse skill data files."""
-from copy import deepcopy
-import re
-import json
+from os import listdir
+from os.path import isfile, join
 import xml.etree.ElementTree as ET
 
 def get_xml_root(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     return root
+
+def get_data_files(directory):
+    return [f for f in listdir(directory) if isfile(join(directory, f))]
+
+def parse_file(filename):
+    print(filename)
+
+source_dir = 'source_files/'
+filenames = get_data_files(source_dir)
+skill_data = [parse_file(join(source_dir, filename)) for filename in filenames]
