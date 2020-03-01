c399e05baa92752bf6f2e2499cb2abb344d4e95a
Ali Turfah <aturfah@gmail.com>
2020-03-01 17:24:38 -0500

    Preliminary building of skillTree

--- src/SkillTree/skillTree.js
+++ src/SkillTree/skillTree.js
@@ -3,39 +3,52 @@ import React, {Component} from 'react';
 import './skillTree.css';
 
 // Data Import
-import masterySkills from '../data/mastery_skills';
-import prereqData from '../data/prereq_data';
 import skillData from '../data/skill_data';
 
-function firstDegSkills() {
-    const output = [];
-    Object.keys(prereqData).forEach(function (key) {
-        const datum = prereqData[key];
-        let firstDeg = false;
+// Helper Functions
+import {firstDegSkills} from '../helpers';
 
-        datum.forEach(function(prereqSkill) {
-            if (masterySkills.includes(prereqSkill._id)) {
-                firstDeg = true;
-            }
-        });
-
-        if (firstDeg === true) {
-            output.push(key);
-        }
-    })
-
-    return output
-}
 
 class SkillTree extends Component {
     constructor(props) {
         super(props);
         this.firstSkills = firstDegSkills()
-        console.log(props);
+        this.buildSkillTree = this.buildSkillTree.bind(this);
     }
 
     buildSkillTree(skillTreeData) {
-        return <div>Here is the Tree!!!</div>
+        const objProps = this.props;
+        const branches = skillTreeData.branches;
+        const output = [];
+        branches.forEach(function (skillBranch) {
+            console.log(skillBranch.name);
+            console.log(skillBranch.skill_data)
+            if (skillBranch.skill_data.length !== 0) {
+                output.push(<h2>{skillBranch.name} Branch</h2>)
+            }
+
+            skillBranch.skill_data.forEach(function (skillDatum) {
+                console.log(skillDatum)
+                let node = [];
+                if (skillDatum.prerequisites.length === 0) {
+                    node.push(<span id={skillDatum._id}>{skillDatum.name}</span>);
+                } else {
+                    node.push(<span id={skillDatum._id}>{skillDatum.name}</span>);
+                }
+
+                console.log(node);
+                if (objProps.skillsChosen.includes(skillDatum._id)) {
+                    node.push(' (Active)')
+                }
+
+                node.push(<br/>)
+                output.push(node)
+            });
+        })
+        return <div>
+            <p>Here is the Tree!!!</p>
+            {output}
+        </div>
     }
 
     render() {
