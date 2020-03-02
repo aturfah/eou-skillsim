c6eec7de223997a5be23fe2df19109fb31bfa241
Ali Turfah <aturfah@gmail.com>
2020-03-02 15:05:59 -0500

    First cut at fixDependencyAdd

--- src/helpers.js
+++ src/helpers.js
@@ -20,3 +20,45 @@ export function firstDegSkills() {
 
     return output
 }
+
+function verifySkillDependencies(chosenSkills) {
+    let newChosenSkills = new Set(chosenSkills);
+    chosenSkills.forEach(function (skillId) {
+        let preReq = prereqData[skillId];
+        console.log(skillId, preReq);
+        if (preReq !== undefined) {
+            preReq.forEach(function (preReqSkill) {
+                newChosenSkills.add(preReqSkill._id)
+            })
+        }
+    });
+    newChosenSkills = [...newChosenSkills]
+
+    if (newChosenSkills.filter(x => !chosenSkills.includes(x)) != []) {
+        return newChosenSkills;
+    } else {
+        return -1
+    }
+}
+
+export function fixSkillDependencyAdd(chosenSkills) {
+    // Check if Prerequisites needed
+    console.log(chosenSkills)
+    let temp = verifySkillDependencies(chosenSkills);
+    while (temp !== -1) {
+        chosenSkills = temp;
+        temp = verifySkillDependencies(temp);
+        console.log(chosenSkills)
+        console.log(temp)
+        throw('doot')
+    }
+
+    // Check if Mastery unlocks other skills
+
+    return chosenSkills
+}
+
+export function fixSkillDependencyDelete(chosenSkills) {
+    console.log(chosenSkills)
+    return chosenSkills
+}
