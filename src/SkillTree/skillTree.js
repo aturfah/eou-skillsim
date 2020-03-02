38fd7c2e7f8793187ca99958ec52ef6be7d9198e
Ali Turfah <aturfah@gmail.com>
2020-03-02 11:42:33 -0500

    Build list of skills for each class with click

--- src/SkillTree/skillTree.js
+++ src/SkillTree/skillTree.js
@@ -14,24 +14,32 @@ class SkillTree extends Component {
         super(props);
         this.firstSkills = firstDegSkills()
         this.buildSkillTree = this.buildSkillTree.bind(this);
+        this._addSkill = this._addSkill.bind(this)
+    }
+
+    _addSkill(skillId) {
+        console.log(skillId);
     }
 
     buildSkillTree(skillTreeData) {
         const objProps = this.props;
+        const addSkillFunc = this._addSkill;
+
         const branches = skillTreeData.branches;
         const output = [];
         branches.forEach(function (skillBranch) {
-            console.log(skillBranch.name);
-            console.log(skillBranch.skill_data)
             if (skillBranch.skill_data.length !== 0) {
                 output.push(<h2>{skillBranch.name} Branch</h2>)
             }
 
+            console.log(skillBranch.name);
+            console.log(skillBranch.skill_data)
             skillBranch.skill_data.forEach(function (skillDatum) {
                 console.log(skillDatum)
                 let node = [];
-                if (skillDatum.prerequisites.length === 0) {
-                    node.push(<span id={skillDatum._id}>{skillDatum.name}</span>);
+                if (skillDatum.prerequisites.length === 0 || true === true) {
+                    node.push(<span id={skillDatum._id}
+                        onClick={() => addSkillFunc(skillDatum._id)}>{skillDatum.name}</span>);
                 } else {
                     node.push(<span id={skillDatum._id}>{skillDatum.name}</span>);
                 }
