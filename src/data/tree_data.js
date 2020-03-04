c12656cb929c67cc002037d43b6395bb8353d70a
Ali Turfah <aturfah@gmail.com>
2020-03-04 13:41:36 -0500

    Preliminary tree data file

---
+++ src/data/tree_data.js
@@ -0,0 +1,30 @@
+const BOX_WIDTH = 300;
+const BOX_HEIGHT = 60;
+const LINE_LENGTH = 40;
+const LINE_THICKNESS = 5;
+var treeData = [
+    [
+        {
+            skillID: 'formula_mastery',
+            baseSkill: true,
+            coords: {x: 0, y: 0}
+        },
+        {
+            skillId: 'fire_formula',
+            baseSkill: false,
+            coords: {x: 0, y: 1}
+        },
+        {
+            skillId: 'ice_formula',
+            baseSkill: false,
+            coords: {x: 0, y: 2}
+        },
+        {
+            skillId: 'volt_formula',
+            baseSkill: false,
+            coords: {x: 0, y: 3}
+        }
+    ]
+]
+
+export default treeData;
