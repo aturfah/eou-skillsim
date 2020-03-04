2bec7d10457fcc11ae7d43478baf42d0a973ffd3
Ali Turfah <aturfah@gmail.com>
2020-03-03 19:44:26 -0500

    Denote clickables as buttons

--- src/Header/header.js
+++ src/Header/header.js
@@ -144,8 +144,8 @@ class Header extends Component {
             {retirementBox}
             {skillPointsInfo}
             <div>
-                <span onClick={() => this._clearSkills()}>CLEAR SKILLS!</span> &nbsp;
-                <span onClick={() => this._resetAll()}>RESET</span>
+                <span onClick={() => this._clearSkills()}>(CLEAR SKILLS)</span> &nbsp;
+                <span onClick={() => this._resetAll()}>(RESET TO DEFAULT)</span>
             </div>
         </div>
     }
