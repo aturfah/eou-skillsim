49f0307443ebdfe3493dd788df6bd01c96d1592d
Ali Turfah <aturfah@gmail.com>
2020-03-04 15:13:50 -0500

    Add numBefore property

--- src/data/tree_data.js
+++ src/data/tree_data.js
@@ -10,91 +10,106 @@ var treeData = [
             skillID: 'fire_formula',
             baseSkill: false,
             coords: {x: 0, y: 1},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'ice_formula',
             baseSkill: false,
             coords: {x: 0, y: 2},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'volt_formula',
             baseSkill: false,
             coords: {x: 0, y: 3},
             numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'inferno_formula',
             baseSkill: false,
             coords: {x: 1, y: 1},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         },
         {
             skillID: 'cocytus_formula',
             baseSkill: false,
             coords: {x: 1, y: 2},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         },
         {
             skillID: 'thor_formula',
             baseSkill: false,
             coords: {x: 1, y: 3},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         },
         {
             skillID: 'flame_formula',
             baseSkill: false,
             coords: {x: 0, y: 4},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'freeze_formula',
             baseSkill: false,
             coords: {x: 0, y: 5},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'spark_formula',
             baseSkill: false,
             coords: {x: 0, y: 6},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'dilution',
             baseSkill: false,
             coords: {x: 1, y: 5},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 3,
         },
         {
             skillID: 'analysis',
             baseSkill: false,
             coords: {x: 0, y: 7},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         },
         {
             skillID: 'pain_formula',
             baseSkill: false,
             coords: {x: 0, y: 8},
-            numAfter: 1
+            numAfter: 1,
+            numBefore: 1,
         },
         {
             skillID: 'firelight',
             baseSkill: false,
             coords: {x: 1, y: 8},
-            numAfter: 2
+            numAfter: 2,
+            numBefore: 1,
         },
         {
             skillID: 'light_formula',
             baseSkill: false,
             coords: {x: 2, y: 8},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         },
         {
             skillID: 'light_tincture',
             baseSkill: false,
             coords: {x: 2, y: 9},
-            numAfter: 0
+            numAfter: 0,
+            numBefore: 1,
         }
     ]
 ]
