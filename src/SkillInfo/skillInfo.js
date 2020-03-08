58837b436d664d88568d293eb3ef969925c69836
Ali Turfah <aturfah@gmail.com>
2020-03-08 19:41:19 -0400

    Preliminary files for the SkillInfoPanel

---
+++ src/SkillInfo/skillInfo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SkillInfoPanel extends Component {
+    constructor(props) {
+        super(props)
+        console.log(props)
+    }
+
+    render() {
+        <div className="SkillInfoPanel">
+            doot
+        </div>
+    }
+}
