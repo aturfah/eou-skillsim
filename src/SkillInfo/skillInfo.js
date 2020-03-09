022d1bf34b20d3eb9ca5f3dcc1d29f4dc7947de9
Ali Turfah <aturfah@gmail.com>
2020-03-08 21:26:34 -0400

    Only render if hovering over something

--- src/SkillInfo/skillInfo.js
+++ src/SkillInfo/skillInfo.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
+import './skillInfo.css'
 
 class SkillInfoPanel extends Component {
-    constructor(props) {
-        super(props)
-        console.log(props)
-    }
-
     render() {
-        <div className="SkillInfoPanel">
+        const activeClassIdx = this.props.activeClassIdx;
+        const activeSkillID = this.props.activeInfo.activeSkillID;
+        const activeSkillBox = this.props.activeInfo.activeSkillBox;
+
+
+        var visibility = 'hidden'
+        if (activeSkillID !== null) {
+            visibility = 'visible'
+        }
+
+        const divStyle = {
+            visibility: visibility
+        }
+
+        console.log(activeClassIdx, activeSkillID, activeSkillBox)
+        console.log(divStyle)
+
+        return <div className="SkillInfoPanel" style={divStyle}>
             doot
         </div>
     }
 }
+
+export default SkillInfoPanel;
