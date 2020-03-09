53ab086f56d3921e6748d84265cf6afbf65b1d70
Ali Turfah <aturfah@gmail.com>
2020-03-09 19:26:51 -0400

    Display something (positioned properly)

--- src/SkillInfo/skillInfo.js
+++ src/SkillInfo/skillInfo.js
@@ -1,27 +1,46 @@
 import React, {Component} from 'react';
 import './skillInfo.css'
 
+import skillData from '../data/skill_data';
+import {parsePX} from '../helpers';
+
 class SkillInfoPanel extends Component {
     render() {
         const activeClassIdx = this.props.activeClassIdx;
         const activeSkillID = this.props.activeInfo.activeSkillID;
         const activeSkillBox = this.props.activeInfo.activeSkillBox;
+        const graphParams = this.props.activeInfo.graphParams;
+        const skillDatum = skillData[activeClassIdx][activeSkillID]
 
-
-        var visibility = 'hidden'
+        var visibility = 'hidden';
+        var left = -1;
+        var top = -1;
         if (activeSkillID !== null) {
             visibility = 'visible'
+            left = parsePX(activeSkillBox.left) +
+                (parsePX(graphParams.BOX_WIDTH) + 2 * parsePX(graphParams.BOX_BORDER_WIDTH)) / 4;
+            top = parsePX(activeSkillBox.top) +
+                (parsePX(graphParams.BOX_HEIGHT) + 2 * parsePX(graphParams.BOX_BORDER_WIDTH)) +
+                parsePX(graphParams.BOX_PADDING / 4);
         }
 
         const divStyle = {
-            visibility: visibility
+            visibility: visibility,
+            left: left,
+            top: top,
+            maxWidth: '200px',
+            maxHeight: '200px',
         }
 
-        console.log(activeClassIdx, activeSkillID, activeSkillBox)
-        console.log(divStyle)
-
         return <div className="SkillInfoPanel" style={divStyle}>
-            doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
+            doot doot doot doot doot doot doot doot doot doot
         </div>
     }
 }
